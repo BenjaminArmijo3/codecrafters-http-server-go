580e0c18d42cc66e733bf5512d940d3997cbdb1a
BenjaminArmijo3 <43712424+BenjaminArmijo3@users.noreply.github.com>
2024-05-22 17:25:50 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -10,12 +10,23 @@ import (
 func GetPath(request string) []string {
 	path := strings.Split(request, " ")
 	path = strings.Split(path[1], "/")
-	return path
+	return path[1:]
+}
+
+func GetHeaders(request string) map[string]string {
+	parts := strings.Split(request, "\r\n\r\n")
+	headers := strings.Split(parts[0], "\r\n")[1:]
+	headersMap := make(map[string]string)
+	for _, header := range headers {
+		key := strings.Split(header, ":")[0]
+		value := strings.Split(header, ":")[1]
+		headersMap[key] = strings.TrimSpace(value)
+	}
+	// fmt.Println(headersMap)
+	return headersMap
 }
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "localhost:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -32,22 +43,17 @@ func main() {
 	conn.Read(buffer)
 
 	path := GetPath(string(buffer))
-	fmt.Println(path)
-	fmt.Println(len(path))
-	if len(path) == 2 {
-		fmt.Println("111")
-		if path[1] != "" {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		} else {
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		}
-	} else if path[1] == "echo" {
-		fmt.Println("222")
-		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", len(path[2]), path[2])))
-	} else {
-		// conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		fmt.Println("404")
+	headers := GetHeaders(string(buffer))
+	// fmt.Println(path)
+	// fmt.Println(len(path))
+	switch path[0] {
+	case "":
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	case "user-agent":
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", len(headers["User-Agent"]), headers["User-Agent"])))
+	case "echo":
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", len(path[1]), path[1])))
+	default:
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
-	// if string(buffer).split()
 }
